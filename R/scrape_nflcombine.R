276b15505212bf797473abe77f4cc89610c3780d
Tan Ho <38083823+tanho63@users.noreply.github.com>
2024-02-25 22:03:55 -0500

    Update scrape_nflcombine.R

--- R/scrape_nflcombine.R
+++ R/scrape_nflcombine.R
@@ -8,6 +8,7 @@ library(httr)
 library(jsonlite)
 
 scrape_combine <- function(season){
+  
   headers = c(
     `User-Agent` = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0',
     `Accept` = '*/*',
